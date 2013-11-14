b2058da4f351170dac5d023abb0ea28f606e77d2
John McLear <john@mclear.co.uk>
2013-11-14 12:56:36 +0000

    log on deviceready

--- www/js/index.js
+++ www/js/index.js
@@ -11,7 +11,7 @@ var app = {
   deviceready: function () {
     // note that this is an event handler so the scope is that of the event
     // so we need to call app.report(), and not this.report()
-    // console.log('deviceready');
+    console.log('deviceready');
     alert = notification.alert;
     prompt = notification.prompt;
 
