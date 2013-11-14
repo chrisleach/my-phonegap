3891d6bf9e3ceb32695d36aa5d56bb546458d64b
John McLear <john@mclear.co.uk>
2013-11-14 13:41:47 +0000

    doh

--- www/js/index.js
+++ www/js/index.js
@@ -11,7 +11,6 @@ var app = {
     // note that this is an event handler so the scope is that of the event
     // so we need to call app.report(), and not this.report()
     console.log('deviceready');
-    /*
     alert = navigator.notification.alert;
     prompt = navigator.notification.prompt;
 
@@ -26,7 +25,6 @@ var app = {
         $('#createNew, #read, #scan').attr('disabled', 'disabled');
       });
     }
-    */
   }
 };
 
